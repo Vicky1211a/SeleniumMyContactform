154dc5a357416b0b791c06a009a07895a57978f8
Vicky1211a <vigneshvickyvkp2000@gmail.com>
2023-03-14 04:15:59 -0700

    updated [class name]

--- src/test/java/TestNGDemo/Testmycontactform2.java
+++ src/test/java/TestNGDemo/Testmycontactform2.java
@@ -11,7 +11,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
-public class Testmycontactform3 {
+public class Testmycontactform2 {
   @Test(dataProvider = "dp")
   
   
